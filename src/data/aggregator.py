c88d59c7f289aef16cf3aba787ad6e68a361f587
Rokas Sabaitis <31926359+skraidantysagurkai@users.noreply.github.com>
2025-05-05 12:32:19 +0300

    Update aggregator.py

--- src/data/aggregator.py
+++ src/data/aggregator.py
@@ -71,8 +71,7 @@ class DataAggregator:
 			if delta <= self.window_two and i <= start_idx:
 			    start_idx = i
 			else:
-		    break
-	            
+		    	    break
 		return (start_idx, inter_idx, sus_idx)
 
 	def compute_window_metrics(self, window, default_dim=10):
