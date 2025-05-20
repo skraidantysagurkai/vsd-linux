ed6caa90ba1d3d4f29b696baaf04cdab217944bb
chingaskirpejas <daiv8314@debian.cloud>
2025-05-20 19:40:01 +0300

    added code for testing

--- paths.py
+++ paths.py
@@ -9,3 +9,7 @@ MODEL_DIR = ROOT_DIR / "models"
 BASH_COMMANDS_PATH = ROOT_DIR / "bash_commands.json"
 
 DOCUMENTATION_DIR = ROOT_DIR / "linux_commands"
+
+TESTING_RES_DIR = ROOT_DIR / "testing_results"
+
+KEY_PATH = '/home/daiv8314/raktas.txt'
